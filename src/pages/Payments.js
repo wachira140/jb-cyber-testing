ed0f2dfc67bca34e60a047e74b207db606ce6236
wachira140 <wachiraw424@gmail.com>
2021-11-25 13:24:41 +0300

    error

--- src/pages/Payments.js
+++ src/pages/Payments.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 import { FaSpinner, FaTrash} from 'react-icons/fa'
